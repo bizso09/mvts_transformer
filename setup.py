20a54df2b016eafe75b238cc3ac46c2ff89a4b4f
bizso09 <bizso09@users.noreply.github.com>
2023-06-04 13:19:56 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mvts_transformer",
+    version="0.0.1",
+    packages=["mvts_transformer"],
+    url="https://github.com/bizso09/mvts_transformer/",
+    project_urls={
+    },
+    license="MIT License",
+    author="@bizso09",
+    author_email="",
+    description="MVTS Transformer",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=[
+        "setuptools",
+        "pandas",
+        "websocket-client",
+        "requests"
+    ],
+)
